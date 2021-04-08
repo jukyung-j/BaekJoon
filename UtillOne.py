56af7dfefd277124ce1e08d7219032dfa0e17ce3
jukyung-j <wjdwnrud123@naver.com>
2021-04-09 02:40:17 +0900

    04-09

--- UtillOne.py
+++ UtillOne.py
@@ -1,13 +1,14 @@
 import math
 
 n, k = map(int, input().split())
-count = 0
-while n>1:
-    if n%k != 0:
-        n-=1
-        count+=1
-    else:
-        n = n//k
-        count+=1
+count=0
+num=0
+while n>k:
+    num = n%k
+    n -= num
+    n //= k
+    count += 1+num
+
+count+=1
 
 print(count)

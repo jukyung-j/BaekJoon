4b2f09bb1043d5ad71b00bbc4286a077c494db7f
jukyung-j <wjdwnrud123@naver.com>
2021-04-10 03:16:32 +0900

    04-10

---
+++ MultipleOrPlus_Greedy.py
@@ -0,0 +1,14 @@
+num = input()
+n=[]
+result=0
+for char in list(num):
+    n.append(int(char))
+
+result = n[0]
+for i in range(1,len(n)):
+    if result==0 or i==0:
+        result += n[i]
+    else:
+        result *= n[i]
+
+print(result)
--- UtillOne.py
+++ UtillOne.py
@@ -3,12 +3,12 @@ import math
 n, k = map(int, input().split())
 count=0
 num=0
-while n>k:
+while n>=k:
     num = n%k
     n -= num
     n //= k
-    count += 1+num
+    count += (1+num)
 
-count+=1
+count += (n-1)
 
 print(count)

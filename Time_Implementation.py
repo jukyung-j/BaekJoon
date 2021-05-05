e451de595b30608311bd1da1eb88bc7b9a627a2f
jukyung-j <wjdwnrud123@naver.com>
2021-05-06 01:38:21 +0900

    05-06

---
+++ Time_Implementation.py
@@ -0,0 +1,20 @@
+import time
+start = time.time()
+
+h = int(input())
+count=0
+for i in range(h+1):
+    if '3' in str(i):
+        count += 3600
+        continue
+    for j in range(60):
+        if '3' in str(j):
+            count += 60
+            continue
+        for k in range(60):
+            if '3' in str(k):
+                count += 1
+
+print(count)
+
+print("time: ",time.time()-start)
